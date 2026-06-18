928acc77747e48f8d47b0d908030765b6909f9b6
Gauri Shankar Yadav <yadavgaurishankar369@gmail.com>
2026-06-18 20:52:07 +0545

    initial comment

---
+++ Day1/python.py
@@ -0,0 +1,47 @@
+"""print("hello world")
+print("hello world",end="")
+print("hello world",end="")
+print("hello world",end="")
+
+print("hello i am gauri")"""
+
+
+
+"""age=12
+print(age)
+my_age=12
+print(type(my_age))
+price=12.50
+print(type(price))
+b=True
+n1=1
+n2=2
+sum=n1+n2
+print(sum)
+sub=n1-n2
+print(sub)
+print(n1**n2)
+print(n1>n2)
+num1=10
+num2=20
+print(num1==num2)
+print(num1>num2)
+a=12
+b=12
+c=b
+#print(a==b)
+#print(a is not b)
+print(a is c)"""
+#a=10
+#print(float(a))
+#print(type(a))
+"""b=10.5
+print(int(b))
+b=float(10)
+print(type(b))"""
+
+number="10"
+a=10
+print(float(a))
+print(int(number))
+
